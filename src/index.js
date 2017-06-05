ee562b5a90b0e45c43772fb7d808569bb56cb3d5
Frank Iriarte <jurrish@gmail.com>
2017-06-05 10:29:12 -0700

    single div component in react

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
+//go find the library called react from our dependencies in our node_modules, and assign it to the variable React. the transpiler will run that file and make sure that index.js has access to those innards.
+//we still reactDom to render components to the DOM
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDom from 'react-dom';
+//1)TODO create a new component. this component should produce some html. aka we are using javacsript functions to return jsx. jsx can't be interpreted by the browser. babel transpiles it for us from the boilerplate. what's the purpose of jsx then? Becuase, that's what produces the html on the DOM. it gets turn into html which then gets placed to the document/DOM.
 
-import App from './components/app';
-import reducers from './reducers';
+const App = () => {
+  //jsx is a dialect of javascript which allows what looks like html to be babeled or transpiled by react.
+  return <div> hi! </div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//2)TODO take this component's generated html and put it on the page (use javascript to put it in the DOM) - we have to tell it WHERE (the second argument) on the DOM's node to insert (it must already exist)
+ReactDom.render(<App />, document.querySelector('.container'));
+
+// (<App />) is different from (App). <App /> is an INSTANCE. App is just a class
